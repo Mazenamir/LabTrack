a3ae3fd1a5aa7c52cb83799a2cd406f97a120fe0
Mazen Amir <mazenamie1001@gmail.com>
2026-03-05 20:59:51 +0200

    V1.1

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,16 @@
 const User = require("../models/User") ;
+const jwt = require("jsonwebtoken");
+
+// ─── Generate JWT Token ───────────────────────────────────────
+const generateToken = (user) => {
+  return jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+};
+
+
 
 //Register 
 
@@ -27,6 +39,49 @@ catch (error) {
 }
 } ;
 
+
+
+// Login
+const login = async (req , res) => {
+    try {
+        const {email , password} = req.body ;
+
+        const user = await User.findOne({email})
+        if (!user){
+            return res.status(400).json({msg :"invailed E-mail or password"})
+
+        }
+
+        if (user.isActive){
+            return res.status(400).json({msg :"Account isn't Active"})
+            
+        }
+
+        //Check the password
+
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // Generate token
+    const token = generateToken(user);
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+    } catch (error) {
+        res.status(500).json({msg : "Server Error" , error : error.message});
+    }
+};
+
 module.exports = {
-    register
+    register , login
 };
